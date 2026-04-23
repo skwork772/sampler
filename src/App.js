5b258f0ff1118e55a9f5f0f97cbbb59aab2f0608
skwork772 <glitstones772@gmail.com>
2026-04-23 21:16:32 +0530

    first2 commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
       <h1>Welcome to my React App!</h1>
       <p>This is a simple React application.</p>
       <p>Feel free to explore and modify the code to your liking.</p>
+      <h1>Enjoy coding!</h1>
+      <h2>Develop the React Coding</h2>
     </div>
   );
 }
